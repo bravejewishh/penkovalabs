46e0f1d8036126ed98b9d85f47a6f886388dbe16
Полина Пенькова <fishofmydream010905@bk.ru>
2025-05-16 02:08:03 +0700

    книги

---
+++ lab6/books.js
@@ -0,0 +1,100 @@
+const books = [
+    {
+        author: 'Тхить Нят Хань',
+        name: 'Древний путь. Белые облака: по следам Будды',
+        publicationyear: 1997,
+        price: 899,
+        bestseller: false,
+        available: true
+    },
+
+    {
+        author: 'Йонге Мингьюр Ринпоче',
+        name: 'Радостная мудрость',
+        publicationyear: 2015,
+        price: 950,
+        bestseller: false,
+        available: true
+    },
+
+    {
+        author: 'Согьял Ринпоче',
+        name: 'Тибетская книга жизни и смерти',
+        publicationyear: 2004,
+        price: 790,
+        bestseller: false,
+        available: false
+    },
+
+    {
+        author: 'Чогьям Трунгпа Ринпоче',
+        name: 'Преодоление духовного материализма',
+        publicationyear: 2009,
+        price: 850,
+        bestseller: true,
+        available: false
+    },
+
+    {
+        author: 'Понлоп Ринпоче',
+        name: 'Мятежный Будда',
+        publicationyear: 2010,
+        price: 699,
+        bestseller: true,
+        available: true
+    },
+
+    {
+        author: 'Его Святейшество Далай-лама XIV',
+        name: 'Искусство быть счастливым. Руководство для жизни',
+        publicationyear: 2009,
+        price: 1150,
+        bestseller: true,
+        available: true
+    },
+
+    {
+        author: 'Сюнрю Судзуки',
+        name: 'Сознание дзен, сознание начинающего',
+        publicationyear: 2013,
+        price: 1090,
+        bestseller: true,
+        available: false
+    },
+
+    {
+        author: 'Чогьям Трунгпа Ринпоче',
+        name: 'Медитация в действии',
+        publicationyear: 1994,
+        price: 930,
+        bestseller: false,
+        available: false
+    },
+
+    {
+        author: 'Стивен Бэчелор',
+        name: 'Буддизм без веры: современное руководство к пробуждению',
+        publicationyear: 1998,
+        price: 890,
+        bestseller: false,
+        available: true
+    },
+
+    {
+        author: 'Тхить Нят Хань',
+        name: 'Сердце учения Будды',
+        publicationyear: 2012,
+        price: 1100,
+        bestseller: false,
+        available: true
+    },
+
+    {
+        author: 'Дзонгсар Джамьянг Кхьенце',
+        name: 'Отчего вы не буддист',
+        publicationyear: 2010,
+        price: 1070,
+        bestseller: true, 
+        available: false
+    },
+];
